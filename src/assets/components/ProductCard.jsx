f10ffc301452d6da3ea09382991e4e4c569ce6dd
rayzathegrave <rdgraaf213@gmail.com>
2023-11-22 13:57:58 +0100

    huiswerk af v2

--- src/assets/components/ProductCard.jsx
+++ src/assets/components/ProductCard.jsx
@@ -1,8 +1,8 @@
-function ProductCard({ProductTitle, ProductPrice, imgSrc, BestSeller}) {
+function ProductCard({ProductTitle, ProductPrice, imgSrc, BestSeller, imgAlt}) {
     return (
         <article>
             <span>{BestSeller}</span>
-            <img src={imgSrc} alt={}/>
+            <img src={imgSrc} alt={imgAlt}/>
             <p>{ProductTitle}</p>
             <h4>{ProductPrice}</h4>
         </article>
